44d26f8f6af7dcf0b2803093d6b2ea959ce44ae8
defectio <defectio916@gmail.com>
2024-04-15 11:13:16 +0900

    LoginFilter -> LogFilter 변경

--- src/main/java/com/defectio/library/login/LogFilter.java
+++ src/main/java/com/defectio/library/login/LogFilter.java
@@ -29,43 +29,43 @@ import org.slf4j.LoggerFactory;
  *   - 필터 체인을 이용하여 여러개의 필터를 사용할 수도 있다.
  *   - HTTP요청 -> WAS -> 필터1 -> 필터2 -> 필터3 -> Dispatcher Servlet -> Controller
  * 
- * [LoginFilter]
+ * [LogFilter]
  *   - 회원(로그인 사용자)만 글쓰기, 수정, 삭제 기능을 사용할 수 있다.(액션 실행 전 로그인 확인 로직 필요)
  *   - 공통의 관심사는 스프링의 AOP로 해결할 수 있지만, 웹과 관련된 공통의 관심사는 서블릿 필터 또는 스프링 인터셉터를 사용하는 것이 좋다.
  *   - 참고 : https://hyuuny.tistory.com/3
  * 
  */
-public class LoginFilter implements Filter {
+public class LogFilter implements Filter {
 
-	private static final Logger logger = LoggerFactory.getLogger(LoginFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
 
 	/**
-	 * 필터 초기화 메서드
+	 * [필터 초기화 메서드]
 	 *   - 서블릿 컨테이너가 생성될 때 호출된다.(default 메서드이기 때문에, 구현하지 않아도 됨.)
 	 */
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		logger.info("LoginFilter init()");
+		logger.info("LogFilter init()");
 	}
 
 	/**
-	 * 필터 종료 메서드
+	 * [필터 종료 메서드]
 	 *   - 서블릿 컨테이너가 종료될 때 호출된다.(default 메서드이기 때문에, 구현하지 않아도 됨.)
 	 */
 	@Override
 	public void destroy() {
-		logger.info("LoginFilter init()");
+		logger.info("LogFilter init()");
 	}
 
 	/**
-	 * 필터 로직 구현부
+	 * [필터 로직 구현부]
 	 *   - HTTP 요청이 오면 doFilter가 호출된다.
-	 *   - ServletRequest request : HTTP 요청이 아닌 경우가지 고려해서 만들어진 인터페이스임
+	 *   - ServletRequest request : HTTP 요청이 아닌 경우 까지 고려해서 만들어진 인터페이스임
 	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		logger.info("LoginFilter doFilter()");
+		logger.info("LogFilter doFilter()");
 		
 		// ServletRequest -> HttpServletRequest casting
 		HttpServletRequest req = (HttpServletRequest) request;
@@ -76,7 +76,7 @@ public class LoginFilter implements Filter {
 			logger.info("REQUEST [{}][{}]", uuid, requestURI);
 			
 			/**
-			 * chain.doFilter(request, response)
+			 * [chain.doFilter(request, response)]
 			 *   - 다음 필터가 있으면 필터를 호출하고, 필터가 없으면 Dispatcher Servlet을 호출한다.
 			 *   - doFilter() 메소드를 호출하지 않으면 다음단계로 진행되지 않음.
 			 */
