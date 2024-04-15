19a11651253aaa8928f5952e7c5002a6f213a446
defectio <defectio916@gmail.com>
2024-04-15 11:12:39 +0900

    로그인 필터 로직 변경, 주석 정리

--- src/main/java/com/defectio/library/login/LoginCheckFilter.java
+++ src/main/java/com/defectio/library/login/LoginCheckFilter.java
@@ -32,7 +32,7 @@ public class LoginCheckFilter implements Filter {
 			"/library/admin/member/loginConfirm",
 			"/library/admin/member/logoutConfirm",
 			"/css/*"};
-	private static final Logger logger = LoggerFactory.getLogger(LoginFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -44,6 +44,10 @@ public class LoginCheckFilter implements Filter {
 		// TODO Auto-generated method stub
 	}
 	
+	/**
+	 * 화이트리스트 패턴과 일치하면, 미로그인 사용자도 접근가능
+	 * 화이트리스트 패턴과 일치하지 않으면, 미로그인 사용자는 접근 불가 -> 로그인 페이지로 이동
+	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
@@ -54,23 +58,39 @@ public class LoginCheckFilter implements Filter {
 		try {
 			logger.info("인증 체크 필터 시작 {}", requestURI);
 
-			// WHITELIST 패턴과 일치하지 않으면
-			if (isLoginCheckPath(requestURI)) {
+			/**
+			 * 로그인이 필요한 서비스 요청인 경우, 로그인 session 여부를 체크한다.
+			 */
+			if (isLoginCheckPath(requestURI)) { // WHITELIST 패턴과 일치하지 않으면~
 				logger.info("인증 체크 로직 실행 {}", requestURI);
 				
+				/**
+				 * [getSession(boolean create) 메소드]
+				 *   - true : to create a new session for this request if necessary
+				 *   - false : to return null if there's no current session
+				 */
 				// 로그인 세션이 없을 경우 -> 미로그인 사용자
 				HttpSession session = req.getSession(false);
 				if (session == null || session.getAttribute("loginedAdminMemberVo") == null) {
 					logger.info("미인증 사용자 요청 {}", requestURI);
 					// 로그인 페이지로 redirect
 					res.sendRedirect("/library/admin/member/loginForm?redirectURI="+requestURI);
-//					res.getRequestDispatcher("page").forward(req, res);
-					return; // 여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
+					// res.getRequestDispatcher("page").forward(req, res);
+					
+					// 미인증 사용자(미로그인 사용자)는 다음으로 진행하지 않고 끝!
+					return; 
+					
 				}
 			}
-
-			// 로그인 사용자만 진행. 
-			// 필터 마지막 -> DispatcherServlet 호출 
+			
+			/**
+			 * (1) WHITELIST 패턴과 일치하지 않음(로그인이 필요한 서비스 요청) + 로그인 한 상태인 경우
+			 * (2) WHITELIST 패턴과 일치(로그인이 필요하지 않은 서비스 요청)하는 경우
+			 *   -> doFilter() 로직 실행
+			 *   -> (마지막 FilterChain인 경우)    DispatcherServlet 호출 or 
+			 *      (다음 FilterChain이 있을 경우)  다음 FilterChain 호출
+			 *   -> HTTP 요청에 따른 Controller 찾아서 로직 실행
+			 */
 			chain.doFilter(req, res);
 		} catch (Exception e) {
 			// 예외 로깅 가능하지만, 톰캣까지 예외를 보내야 함
@@ -78,7 +98,6 @@ public class LoginCheckFilter implements Filter {
 		} finally {
 			logger.info("인증 체크 필터 종료 {}", requestURI);
 		}
-
 	}
 	
 	/**
