56044cab11ab1267c8765708d1ee652ab946c3cc
defectio916 <defectio916@gmail.com>
2025-12-13 08:41:23 +0900

    정리

--- src/main/java/com/defectio/library/admin/AdminHomeController.java
+++ src/main/java/com/defectio/library/admin/AdminHomeController.java
@@ -4,18 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller		// 프로젝트가 실행될 때 IoC컨테이너에 bean 객체로 생성된다.
 @RequestMapping("/admin")  
-// /admin에 대한 요청을 AdminHomeController에서 처리한다
+// "/admin"에 대한 요청을 AdminController가 처리한다. 
+// @RequestMapping("/admin")를 명시하지 않으면 AdminController는 클라이언트의 모든 요청을 처리할 수 있는 컨트롤러가 된다.
 public class AdminHomeController {
 
 	/**
-	 * 
-	 * @return
-	 */
-	/*
 	 * value : /admin, /admin/ 에 대한 요청을 home() 에서 처리한다.
 	 * method : 요청 정보 전송방식(get, post)
+	 * @return
 	 */
 	@RequestMapping(value={"", "/"}, method=RequestMethod.GET)
 	public String home() {
