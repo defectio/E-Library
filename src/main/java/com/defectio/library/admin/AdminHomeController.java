0bf3a8fdc0983b80cc3e9982fc3ecb701859cca9
defectio916 <defectio916@gmail.com>
2026-06-10 06:57:47 +0900

    정리

--- src/main/java/com/defectio/library/admin/AdminHomeController.java
+++ src/main/java/com/defectio/library/admin/AdminHomeController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller		// 프로젝트가 실행될 때 IoC컨테이너에 bean 객체로 생성된다.
-@RequestMapping("/admin")  
-// "/admin"에 대한 요청을 AdminController가 처리한다. 
-// @RequestMapping("/admin")를 명시하지 않으면 AdminController는 클라이언트의 모든 요청을 처리할 수 있는 컨트롤러가 된다.
+
+/**
+ * @Controller : 프로젝트가 실행될 때 IoC컨테이너에 bean 객체로 생성된다.
+ * 
+ * @RequestMapping("/admin") : "/admin"에 대한 요청을 AdminController가 처리한다. 
+ * @RequestMapping("/admin")를 명시하지 않으면 AdminController는 클라이언트의 모든 요청을 처리할 수 있는 컨트롤러가 된다.
+ */
+@Controller
 public class AdminHomeController {
 
 	/**
 	 * value : /admin, /admin/ 에 대한 요청을 home() 에서 처리한다.
 	 * method : 요청 정보 전송방식(get, post)
+	 * 
+	 * @RequestMapping(value={"", "/"}, method=RequestMethod.GET)
+	 * @RequestMapping(value={"", "/"}) : method default가 RequestMethod.GET
+	 * @RequestMapping({"", "/"}) : 인자 1개일 경우, value key 생략 가능
 	 * @return
 	 */
 	@RequestMapping(value={"", "/"}, method=RequestMethod.GET)
